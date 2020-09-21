8ffb5a8de9000c9e16a16ab2094f55e43a7cfe1e
Towhid Zaman <32498111+towhid1zaman@users.noreply.github.com>
2020-09-21 22:20:29 +0600

    mod-inverse

--- MOD inverse.cpp
+++ MOD inverse.cpp
@@ -1,2 +1,18 @@
-template<typename T> T bigmod(T a,T b, long long m){T ans=1; while(b>0){ if(b%2==1) ans=(ans*a)%m; b/=2; a=(a*a)%m; } return ans%m; }
-template <class T> inline T modinverse(T a,T M){return bigmod(a,M-2,M);}   // M is prime}
+ll bigmod(ll a, ll e, ll m) {
+	ll ret = 1LL;
+	ll aa = (ll)a;
+	while (e) {
+		if (e&1) {
+			ret *= aa;
+			if (ret > m)
+				ret %= m;
+			e--;
+		}
+		aa *= aa;
+		e /= 2;
+		if (aa > m)
+			aa %= m;
+	}
+	return ret;
+}
+template <class T> inline T modinverse(T a,T M){return bigmod(a,M-2,M);}   // M is prime
