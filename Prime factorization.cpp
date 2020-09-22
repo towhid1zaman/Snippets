25047b7c9434a39cb5d5162881f4550353b71210
Towhid Zaman <32498111+towhid1zaman@users.noreply.github.com>
2020-09-22 11:03:08 +0600

    prime factorization vector<pii>

---
+++ Prime factorization.cpp
@@ -0,0 +1,16 @@
+//prime factorization
+vector<pair<ll,ll>>factorize(ll a){
+    vector<pair<ll,ll> > pf;
+    for (int i = 2; i * i <= a; ++i) {
+        if (a % i) continue;
+        pf.emplace_back(i, 0);
+        while (a % i == 0) {
+            ++pf.back().second;
+            a /= i;
+        }
+    }
+    if (a > 1) {
+        pf.emplace_back(a, 1);
+    }
+    return pf;
+}
