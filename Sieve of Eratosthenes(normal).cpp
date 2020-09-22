2af7e59a78429c181511ac1f91891ce5169b57f4
Towhid Zaman <32498111+towhid1zaman@users.noreply.github.com>
2020-09-22 11:47:51 +0600

    sieve_normal

--- Sieve of Eratosthenes(normal).cpp
+++ Sieve of Eratosthenes(normal).cpp
@@ -1,16 +1,24 @@
 //quick sieve
+ll chek[maxn];
 bool checkprime[maxn + 5];
 vll prime;
-void sieve() {///false means prime
+void sieve() {///true means prime
+	fill(checkprime,1);
     prime.push_back(2);
-    checkprime[0]=true;
-    checkprime[1]=true;
-    for(ll i = 4; i<=maxn; i+=2)checkprime[i] = true;
+    checkprime[0]=0;
+    checkprime[1]=0;
+    for(ll i = 4; i<=maxn; i+=2)checkprime[i] = 0;
     for (ll i = 3; i <= maxn; i += 2) {
-        if (!checkprime[i]) {
+        if (checkprime[i]) {
             prime.push_back(i);
             for (ll j = i * i; j <= maxn; j += (i + i))
-                checkprime[j] = true;
+                checkprime[j] = 0;
         }
     }
+    //interval
+    chek[0]=0;
+    for(int i = 1; i<=maxn; i++){
+    	chek[i] = chek[i-1] + checkprime[i-1];
+    }
+
 }
