64b4de7bc47036b130dc1bf332f67e20d8022b77
Towhid Zaman <32498111+towhid1zaman@users.noreply.github.com>
2020-09-26 20:27:56 +0600

    O(n+2*m) = O(n+m)

--- BFS (Breadth-first search).cpp
+++ BFS (Breadth-first search).cpp
@@ -1,13 +1,15 @@
+//dist calculate lavels, and ways calculate number of ways of same shortest path from source to any other node
+
 std::vector<int>adj[maxn];
 bool vis[maxn];
-int dist[maxn];
-int par[maxn];
+int dist[maxn], par[maxn], ways[maxn];
 
 void RESET(){
 	for(int i = 0; i<maxn; i++){
 		vis[i] = 0;
 		dist[i] = 0;
 		par[i] = 0;
+		ways[i] = 0;
 		adj[i].clear();
 	}
 }
@@ -16,17 +18,24 @@ void bfs(int source){
 	queue<int>q;
 	q.push(source);
 	vis[source] = 1, dist[source] = 0, par[source] = -1;
+	ways[source] = 1;
 	while(!q.empty()){
-		int u = q.front();
+		int node = q.front();
 		q.pop();
-		for(auto v:adj[u]){
-			if(vis[v])continue;
-			q.push(v);
-			vis[v] = 1, dist[v] = dist[u] + 1, par[v] = u;
+		for(auto child:adj[node]){
+			if(!vis[child]){
+				q.push(child);
+				vis[child] = 1, dist[child] = dist[node] + 1, par[child] = node;
+				ways[child] += ways[node];
+			}
+			else{
+				if(dist[node]+1 == dist[child]){
+					ways[child] += ways[node];
+				}
+			}
 		}
 	}
 }
-//first call bfs()
 void path_print(int destination){
 	std::vector<int>path;
 	int u = destination;
