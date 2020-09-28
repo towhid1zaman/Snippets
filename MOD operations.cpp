dbc6d9746f6f8dd352eb07280c09e16f0cdb35d3
Towhid Zaman <32498111+towhid1zaman@users.noreply.github.com>
2020-09-28 21:43:57 +0600

    add,mult,subtract

---
+++ MOD operations.cpp
@@ -0,0 +1,3 @@
+inline ll add(ll a,ll b) {return ((a % mod) + (b % mod)) % mod;}
+inline ll sub(ll a,ll b) {return ((a % mod) - (b % mod) + mod) % mod;}
+inline ll mul(ll a,ll b) {return ((a % mod) * (b % mod)) % mod;}
