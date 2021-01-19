ed258c26b950b1a07216029b2367e1c545b05c81
Towhid Zaman <32498111+towhid1zaman@users.noreply.github.com>
2021-01-19 23:48:30 +0600

    Update Sieve of Eratosthenes(linear).cpp

--- Sieve of Eratosthenes(linear).cpp
+++ Sieve of Eratosthenes(linear).cpp
@@ -9,3 +9,28 @@ void sieve() {//true means prime
             primes.push_back(i);
         }
 }
+
+//another
+vector<int> smallest_factor;
+vector<bool> isp;
+vector<int> primes;
+void sieve(int maximum) {
+    maximum = max(maximum, 1);
+    smallest_factor.assign(maximum + 1, 0);
+    isp.assign(maximum + 1, true);
+    isp[0] = isp[1] = false;
+    primes = {};
+ 
+    for (int p = 2; p <= maximum; p++)
+        if (isp[p]) {
+            smallest_factor[p] = p;
+            primes.push_back(p);
+ 
+            for (int64_t i = int64_t(p) * p; i <= maximum; i += p)
+                if (isp[i]) {
+                    isp[i] = false;
+                    smallest_factor[i] = p;
+                }
+        }
+}
+ 
