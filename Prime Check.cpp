e90a527acfc53d6cacecb1a4be132dbde5c62710
Towhid Zaman <32498111+towhid1zaman@users.noreply.github.com>
2021-05-17 11:06:49 +0600

    prime_check

--- Prime Check.cpp
+++ Prime Check.cpp
@@ -1,11 +1,12 @@
-//check prime
-bool isprime(ll n){
-    if(n==2)return true;
-    if(n<2 or n%2==0)return false;
-    for(ll p = 3; p*p<=n;p+=2){
-        if(n%p==0){
-            return false;
-        }
-    }
-    return true;
+bool isprime(ll n) { 
+  if (n <= 1) return false; 
+  if (n <= 3) return true; 
+
+  if (n % 2 == 0 || n % 3 == 0) return false; 
+
+  for (ll i = 5; i * i <= n; i += 6) 
+    if (n % i == 0 || n % (i+2) == 0) 
+      return false; 
+
+  return true; 
 }
