e6d7483ee7e2b864c8c0e0259b1bf346eaa89bcb
Towhid Zaman <32498111+towhid1zaman@users.noreply.github.com>
2021-05-17 12:02:23 +0600

    NT

--- Number Therory.cpp
+++ Number Therory.cpp
@@ -110,4 +110,17 @@ namespace Number_Theory{
         if(n!=1)ans*=2;
         return ans;
     }
+    //All divisors of a number
+    std::vector<ll>getdivs(ll n){
+      std::vector<ll>divs;
+        for(ll i = 1; i * i <= n; i++) {
+          if (n % i == 0) {
+            divs.push_back(i);
+            if(i != (n/i))divs.push_back(n/i);
+          }
+      }
+      sort(divs.begin(), divs.end());
+      return divs;
+    }
+
 }using namespace Number_Theory;
