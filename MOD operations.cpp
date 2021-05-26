3768de1c04319b4646ff9d4bec866c1042a95add
Towhid Zaman <32498111+towhid1zaman@users.noreply.github.com>
2021-05-27 01:24:29 +0600

    Delete MOD operations.cpp

--- MOD operations.cpp
+++
@@ -1,3 +0,0 @@
-inline ll add(ll a,ll b) {return ((a % mod) + (b % mod)) % mod;}
-inline ll sub(ll a,ll b) {return ((a % mod) - (b % mod) + mod) % mod;}
-inline ll mul(ll a,ll b) {return ((a % mod) * (b % mod)) % mod;}
