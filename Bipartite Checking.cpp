1b867751e8bbd49ab900ecb5b5912a801b20cd7e
Towhid Zaman <32498111+towhid1zaman@users.noreply.github.com>
2021-06-23 03:47:09 +0600

    Bipartite

--- Bipartite Checking.cpp
+++ Bipartite Checking.cpp
@@ -8,13 +8,13 @@ namespace Bipartite{
         vis.assign(n, 0);
         col.assign(n, -1);
     }
-
-    void dfs(int x, int curcol, int& not_bipartite){
+ 
+    void dfs(int x, int curcol, int& bi_tie){
         vis[x] = 1;
         col[x] = curcol;
         for(auto v : adj[x]){
-            if(col[v] == -1)dfs(v,curcol ^ 1, not_bipartite);
-            else if(col[v] == curcol)not_bipartite = 0;
+            if(col[v] == -1)dfs(v,curcol ^ 1, bi_tie);
+            else if(col[v] == curcol)bi_tie = 0;
         }
     }
     int is_bipartie(int v){
@@ -22,6 +22,6 @@ namespace Bipartite{
         dfs(0,0,bi_tie);
         return bi_tie;
     }
-
+ 
     //check range
 }using namespace Bipartite;
